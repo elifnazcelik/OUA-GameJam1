f69555d35a2b54784510e7e9cd345b9f9c522e27
unknown <kodmachine@gmail.com>
2024-03-16 15:42:44 +0300

    slope ground

--- GameJamOUA/Assets/Scripts/DogController.cs
+++ GameJamOUA/Assets/Scripts/DogController.cs
@@ -5,6 +5,7 @@ public class DogController : MonoBehaviour
     public float speed = 5f;
     public float jumpForce = 10f;
     private bool isGrounded;
+    private bool isSlopingGround;
     private Rigidbody2D rb;
     private Animator anim;
 
@@ -27,15 +28,28 @@ public class DogController : MonoBehaviour
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
 
         if (Input.GetButtonDown("Jump") && isGrounded) rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        
+        if (isSlopingGround) 
+        {
+            if (Input.GetKeyDown(KeyCode.D))
+                transform.Rotate(0,0,45);
+            if (Input.GetKeyDown(KeyCode.A))
+                transform.Rotate(0,0,-45);
+            //if (moveInput < 0) transform.eulerAngles = new Vector3(0, 180, 0);
+            //else if (moveInput > 0) transform.eulerAngles = new Vector3(0, 0, 0);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Ground")) isGrounded = true;
+        if (collision.collider.CompareTag("SlopingGround")) isSlopingGround = true;
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Ground")) isGrounded = false;
     }
+
+    
 }
