39e6cb8e59b0403eab259234ed14941cdb2915f6
Samet Kosmaz <ksmzsmt@gmail.com>
2024-03-16 22:13:03 +0300

    city scene(demo) added

---
+++ GameJamOUA/Assets/Scripts/deadZone.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class deadZone : MonoBehaviour
+{
+    [SerializeField] GameObject _canvas;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Time.timeScale = 0;
+            _canvas.SetActive(true);
+        }
+    }
+}
