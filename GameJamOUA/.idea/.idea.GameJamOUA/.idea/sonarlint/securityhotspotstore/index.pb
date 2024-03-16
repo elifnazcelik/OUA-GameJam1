ae5643cf8cf640448fab4011307cd08717684482
Mert Ayhan <com9@hotmail.com.tr>
2024-03-16 14:47:28 +0300

    Dog animation

---
+++ GameJamOUA/.idea/.idea.GameJamOUA/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,3 @@
+
+O
+Assets/Scripts/DogController.cs,9\5\952187ad371b703869ebf8f6ae15782ae0cf9ee8
--- GameJamOUA/Assets/Scripts/DogController.cs
+++ GameJamOUA/Assets/Scripts/DogController.cs
@@ -1,20 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DogController : MonoBehaviour
 {
-    private CharacterController characterController;
-    public float Speed = 1.0f;
+    public float speed = 5f;
+    public float jumpForce = 10f;
+    private bool isGrounded;
+    private Rigidbody2D rb;
+    private Animator anim;
 
-    void Start()
+    private void Start()
     {
-        characterController = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    private void Update()
     {
-        Vector2 move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        characterController.Move(move * Time.deltaTime * Speed);
+        var moveInput = Input.GetAxis("Horizontal");
+        
+        if (moveInput != 0) anim.SetBool("isRunning", true);
+        else anim.SetBool("isRunning", false);
+        
+        if (moveInput < 0) transform.eulerAngles = new Vector3(0, 180, 0);
+        else if (moveInput > 0) transform.eulerAngles = new Vector3(0, 0, 0);
+        
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && isGrounded) rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground")) isGrounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground")) isGrounded = false;
     }
 }
