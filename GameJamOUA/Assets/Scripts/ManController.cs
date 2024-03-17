5a43bc9068c601e1726858044f2831a8eae56ef8
Mert Ayhan <com9@hotmail.com.tr>
2024-03-17 12:40:19 +0300

    bird mechanic

--- GameJamOUA/Assets/Scripts/ManController.cs
+++ GameJamOUA/Assets/Scripts/ManController.cs
@@ -6,6 +6,10 @@ public class ManController : MonoBehaviour
     public float speed = 1.0f;
     private Animator anim;
     [SerializeField] GameObject gameEndScreen;
+    public float cooldown = 3f;
+    private bool isCooldown = false;
+    private bool isNearDog = false;
+    public GameObject shield;
 
     private void Start()
     {
@@ -16,6 +20,14 @@ public class ManController : MonoBehaviour
     private void Update()
     {
         transform.Translate(speed * Time.deltaTime, 0, 0);
+        
+        if (Input.GetKeyDown(KeyCode.X) && !isCooldown && isNearDog)
+        {
+            anim.SetTrigger("attack");
+            isCooldown = true;
+            shield.SetActive(true);
+            Invoke("ResetCooldown", cooldown);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D other)
@@ -23,6 +35,13 @@ public class ManController : MonoBehaviour
         if (other.gameObject.CompareTag("MovingGround"))
         {
             speed = 2f;
+        }
+        
+        if (other.gameObject.CompareTag("Bird"))
+        {
+            speed = 0;
+            anim.SetTrigger("died");
+            Invoke("EndGame", 1);
         }
     }
 
@@ -47,6 +66,7 @@ public class ManController : MonoBehaviour
         {
             speed = 0;
             anim.SetBool("isWalking", false);
+            isNearDog = true;
         }
     }
 
@@ -56,6 +76,7 @@ public class ManController : MonoBehaviour
         {
             anim.SetBool("isWalking", true);
             speed = 1.0f;
+            isNearDog = false;
         }
     }
     
@@ -63,5 +84,11 @@ public class ManController : MonoBehaviour
     {
         gameEndScreen.SetActive(true);
         Time.timeScale = 0;
+    }
+
+    void ResetCooldown()
+    {
+        isCooldown = false;
+        shield.SetActive(false);
     }
 }
